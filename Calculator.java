2f253b9c9cba0b6e2b9155dd1319cfd1945505a9
Pawan G Tekale <121091609+Pawangtekale@users.noreply.github.com>
2025-11-24 18:26:08 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+class Calculator
+{
+	public static double isAddition(double a,double b){
+		double sum=a+b;
+		return sum;
+		
+		
+	}
+	public static void main(String[] args) 
+	{
+		//isAddition(2,2);
+		System.out.println(isAddition(2,2));
+		
+	}
+}
