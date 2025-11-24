db8e27e1fcaf4b2f9633354156c411116359023a
Pawan G Tekale <121091609+Pawangtekale@users.noreply.github.com>
2025-11-24 18:45:40 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,13 +3,17 @@ class Calculator
 	public static double isAddition(double a,double b){
 		double sum=a+b;
 		return sum;
-		
-		
+	}
+	public static double isSubstraction(double a,double b){
+		double sub=a-b;
+		return sub;
 	}
 	public static void main(String[] args) 
 	{
 		//isAddition(2,2);
 		System.out.println(isAddition(2,2));
+		System.out.println(isSubstraction(4,2));
 		
 	}
 }
+
